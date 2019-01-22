c4b122036454b3f2b2f79365457b043089ed0adb
zhou.jiangyuan <zhou.jiangyuan@cesgroup.com.cn>
2019-01-22 10:20:07 +0800

    no message

--- ZCKit/Additional/ZCMonitorService.h
+++ ZCKit/Additional/ZCMonitorService.h
@@ -8,9 +8,12 @@
 
 #import <Foundation/Foundation.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 typedef NS_OPTIONS(NSUInteger, ZCMonitorType) {
     ZCMonitorTypeNone   = 0,  /**< 用于获取监听类型 */
-    ZCMonitorTypeTest   = 1 << 0,
+    ZCMonitorTypeTest1  = 1 << 0,
+    ZCMonitorTypeTest2  = 1 << 1,
 };
 
 typedef NS_ENUM(NSInteger, ZCMonitorPriority) {
@@ -20,13 +23,12 @@ typedef NS_ENUM(NSInteger, ZCMonitorPriority) {
     ZCMonitorPriorityHigh     = 3,  /**< 高优先级 */
 };
 
-NS_ASSUME_NONNULL_BEGIN
 
 @interface ZCMonitorBroadcast : NSObject
 
 @property (nonatomic, assign, readonly) int rank;  /**< 广播接收者先后排名，从0开始 */
 
-@property (nonatomic, assign, readonly) ZCMonitorType type;  /**< 广播类型 */
+@property (nonatomic, assign, readonly) ZCMonitorType type;  /**< 广播类型，单值或者复合值 */
 
 @property (nonatomic, assign, readonly) ZCMonitorPriority priority;  /**< 广播优先级 */
 
@@ -34,13 +36,13 @@ NS_ASSUME_NONNULL_BEGIN
 
 @property (nullable, nonatomic, strong, readonly) id object;  /**< 广播传递的对象 */
 
-@property (nonatomic, strong, readonly) NSDictionary *infos;  /**< 传递的信息字典 */
+@property (nonatomic, strong, readonly) NSDictionary *infos;  /**< 广播传递的信息字典 */
 
-@property (nonatomic, strong, readonly) NSArray <NSString *>*ids;  /**< 传递的信息数组 */
+@property (nonatomic, strong, readonly) NSArray <NSString *>*ids;  /**< 广播传递的信息数组 */
 
-+ (instancetype)broadcastType:(ZCMonitorType)type issuer:(nullable id)issuer;  /**< 初始化 */
++ (instancetype)broadcastType:(ZCMonitorType)type issuer:(nullable id)issuer;  /**< 广播初始化 */
 
-- (void)resetObject:(nullable id)object ids:(nullable NSArray <NSString *>*)ids infos:(nullable NSDictionary *)infos;  /**< 赋值 */
+- (void)resetObject:(nullable id)object ids:(nullable NSArray <NSString *>*)ids infos:(nullable NSDictionary *)infos;  /**< 广播赋值 */
 
 @end
 
@@ -52,8 +54,11 @@ NS_ASSUME_NONNULL_BEGIN
 - (ZCMonitorType)monitorForwardBroadcast:(ZCMonitorBroadcast *)broadcast;
 
 @optional
-/** 返回对特定类型的广播接收的优先级，broadcast.type在此为单值 */
+/** 返回对特定类型的广播接收的优先级，broadcast.type在此为单值，在注册的时候调用一次，默认为Normal */
 - (ZCMonitorPriority)monitorPriorityWithType:(ZCMonitorType)type;
+
+/** 返回此类型广播是否是懒广播，broadcast.type在此为单值，在注册的时候调用一次，默认为NO */
+- (BOOL)monitorLazyReceiveWithType:(ZCMonitorType)type;
 
 @end
 
@@ -65,6 +70,12 @@ NS_ASSUME_NONNULL_BEGIN
 
 /** 发布广播，type在此可以是复合值来同时发送多个广播 & 复合值发送顺序按子type从小到大 */
 + (void)issue_broadcast:(ZCMonitorBroadcast *)broadcast;
+
+/** 开始接收聚集的懒广播，并且进入活跃状态，开始时时接收懒广播，type在此可以是复合值，设置type为None时则认为是所有类型 */
++ (void)open_lazyReceive:(id <ZCMonitorProtocol>)listener type:(ZCMonitorType)type;
+
+/** 停止接收懒广播，并且进入非活跃状态，暂停接收懒广播，type在此可以是复合值，设置type为None时则认为是所有类型 */
++ (void)close_lazyReceive:(id <ZCMonitorProtocol>)listener type:(ZCMonitorType)type;
 
 /** 移除监听 */
 + (void)remove_listener:(id <ZCMonitorProtocol>)listener;
--- ZCKit/Additional/ZCMonitorService.m
+++ ZCKit/Additional/ZCMonitorService.m
@@ -7,8 +7,9 @@
 //
 
 #import "ZCMonitorService.h"
+#import <UIKit/UIKit.h>
 
-#define MONITOR_USE_PRIORITY  0  /* 是否使用优先级，暂不使用 */
+#define MONITOR_USE_PRIORITY_LAZY  0  /* 是否使用优先级&懒广播，暂不使用 */
 
 #pragma mark - Class - ZCMonitorBroadcast
 @implementation ZCMonitorBroadcast
@@ -50,6 +51,27 @@
 @end
 
 
+#pragma mark - Class - ZCMonitorLazy
+@interface ZCMonitorLazy : NSObject
+
+@property (nonatomic, assign) BOOL isOpen;  /**< 懒广播时是否是允许时时接收 */
+
+@property (nonatomic, strong) NSMutableArray <ZCMonitorBroadcast *>*cache;  /**< 懒广播缓存 */
+
+@end
+
+@implementation ZCMonitorLazy
+
+- (NSMutableArray <ZCMonitorBroadcast *>*)cache {
+    if (!_cache) {
+        _cache = [NSMutableArray array];
+    }
+    return _cache;
+}
+
+@end
+
+
 #pragma mark - Class - ZCMonitorListener
 @interface ZCMonitorListener : NSObject
 
@@ -60,6 +82,10 @@
 @property (nonatomic, assign) NSUInteger mask1;  /**< mask * 2e+0 */
 
 @property (nonatomic, assign) NSUInteger mask2;  /**< mask * 2e+1 */
+
+@property (nonatomic, assign) NSUInteger mask3;  /**< mask * 2e+0 */
+
+@property (nonatomic, strong) NSMutableDictionary <NSNumber *, ZCMonitorLazy *>*lazyMap;  /**< lazy类型映射 */
 
 @end
 
@@ -72,13 +98,58 @@
     return self;
 }
 
+- (void)open_caches:(ZCMonitorType)types {
+    if (MONITOR_USE_PRIORITY_LAZY) {
+        NSUInteger n = types; NSUInteger i = 0;
+        if (types == ZCMonitorTypeNone) n = _mask3;
+        while (n > 0) {
+            NSUInteger y = n % 2; n >>= 1;
+            if (y == 1) {
+                ZCMonitorType type = 1 << i;
+                ZCMonitorLazy *lazy = [self addMapType:type];
+                lazy.isOpen = YES;
+                [self issue_lazy:lazy];
+            }
+            i += 1;
+        }
+    }
+}
+
+- (void)close_caches:(ZCMonitorType)types {
+    if (MONITOR_USE_PRIORITY_LAZY) {
+        NSUInteger n = types; NSUInteger i = 0;
+        if (types == ZCMonitorTypeNone) n = _mask3;
+        while (n > 0) {
+            NSUInteger y = n % 2; n >>= 1;
+            if (y == 1) {
+                [self addMapType:(1 << i)].isOpen = NO;
+            }
+            i += 1;
+        }
+    }
+}
+
+- (BOOL)joinLazyBroadcast:(ZCMonitorBroadcast *)broadcast {
+    ZCMonitorLazy *lazy = [self addMapType:broadcast.type];
+    if (!lazy.isOpen) {
+        [lazy.cache addObject:broadcast];
+        return YES;
+    }
+    return NO;
+}
+
+- (ZCMonitorPriority)listenPriority:(ZCMonitorType)type {
+    return (2 * ((self.mask2 & type) ? 1 : 0) + ((self.mask1 & type) ? 1 : 0));
+}
+
+#pragma mark - Private
 - (void)setListener:(id<ZCMonitorProtocol>)listener {
     _listener = listener;
     if ([listener respondsToSelector:@selector(monitorForwardBroadcast:)]) {
         _listenType = [listener monitorForwardBroadcast:[ZCMonitorBroadcast broadcastType:ZCMonitorTypeNone issuer:nil]];
     }
-    if (MONITOR_USE_PRIORITY) {
-        _mask1 = 0; _mask2 = 0;
+    if (MONITOR_USE_PRIORITY_LAZY) {
+        _mask1 = 0; _mask2 = 0; _mask3 = 0; [_lazyMap removeAllObjects];
         NSUInteger n = _listenType; NSUInteger i = 0;
         while (n > 0) {
             NSUInteger y = n % 2; n >>= 1;
@@ -91,16 +162,46 @@
                 } else {
                     _mask1 = _mask1 | type;
                 }
+                if ([listener respondsToSelector:@selector(monitorLazyReceiveWithType:)] && [listener monitorLazyReceiveWithType:type]) {
+                    _mask3 = _mask3 | type;
+                }
             }
             i += 1;
         }
     }
 }
 
+- (void)issue_lazy:(ZCMonitorLazy *)lazy {
+    for (ZCMonitorBroadcast *subbro in lazy.cache) {
+        if ([self.listener respondsToSelector:@selector(monitorForwardBroadcast:)]) {
+            [subbro resetRank:0 priority:[self listenPriority:subbro.type]];
+            [self.listener monitorForwardBroadcast:subbro];
+        }
+    }
+    [lazy.cache removeAllObjects];
+}
+
+- (ZCMonitorLazy *)addMapType:(ZCMonitorType)type {
+    NSNumber *number = [NSNumber numberWithUnsignedInteger:type];
+    ZCMonitorLazy *lazy = [self.lazyMap objectForKey:number];
+    if (!lazy) {
+        lazy = [[ZCMonitorLazy alloc] init];
+        [self.lazyMap setObject:lazy forKey:number];
+    }
+    return lazy;
+}
+
+- (NSMutableDictionary <NSNumber *, ZCMonitorLazy *>*)lazyMap {
+    if (!_lazyMap) {
+        _lazyMap = [NSMutableDictionary dictionary];
+    }
+    return _lazyMap;
+}
+
 @end
 
 
-#pragma mark - Class - ZCMonitor
+#pragma mark - Class - ZCMonitorService
 @interface ZCMonitorService ()
 
 @property (nonatomic, strong) NSMutableArray <ZCMonitorListener *>*listeners;
@@ -126,23 +227,31 @@
 }
 
 - (void)dealloc {
-    [self.listeners removeAllObjects];
+    [_listeners removeAllObjects];
 }
 
-- (ZCMonitorPriority)listenPriority:(ZCMonitorListener *)listen mask:(NSUInteger)mask {
-    return 2 * ((listen.mask2 & mask) ? 1 : 0) + ((listen.mask1 & mask) ? 1 : 0);
+- (ZCMonitorListener *)find_listener:(id <ZCMonitorProtocol>)listener {
+    if (!listener) return nil;
+    ZCMonitorListener *find = nil;
+    for (ZCMonitorListener *lis in self.listeners) {
+        if (lis.listener && [lis.listener isEqual:listener]) {
+            find = lis; break;
+        }
+    }
+    return find;
 }
 
 - (void)issue_map:(ZCMonitorBroadcast *)subbro {
     NSMutableArray *maps = [NSMutableArray arrayWithCapacity:self.listeners.count];
     for (ZCMonitorListener *listen in self.listeners) {
         if (listen.listener && (listen.listenType & subbro.type)) {
-            [maps addObject:listen];
+            if ((listen.mask3 & subbro.type) && [listen joinLazyBroadcast:subbro]) {}
+            else {[maps addObject:listen];}
         }
     }
     [maps sortUsingComparator:^NSComparisonResult(ZCMonitorListener *_Nonnull obj1, ZCMonitorListener *_Nonnull obj2) {
-        ZCMonitorPriority pri1 = [self listenPriority:obj1 mask:subbro.type];
-        ZCMonitorPriority pri2 = [self listenPriority:obj2 mask:subbro.type];
+        ZCMonitorPriority pri1 = [obj1 listenPriority:subbro.type];
+        ZCMonitorPriority pri2 = [obj2 listenPriority:subbro.type];
         if (pri1 == pri2) return NSOrderedSame;
         else if (pri1 > pri2) return NSOrderedAscending;
         else return NSOrderedDescending;
@@ -150,7 +259,7 @@
     int rank = 0;
     for (ZCMonitorListener *listen in maps) {
         if ([listen.listener respondsToSelector:@selector(monitorForwardBroadcast:)]) {
-            [subbro resetRank:rank priority:[self listenPriority:listen mask:subbro.type]];
+            [subbro resetRank:rank priority:[listen listenPriority:subbro.type]];
             [listen.listener monitorForwardBroadcast:subbro];
             rank += 1;
         }
@@ -165,11 +274,15 @@
     NSUInteger n = broadcast.type; NSUInteger i = 0;
     while (n > 0) {
         NSUInteger y = n % 2; n >>= 1;
-        if (y == 1) [subbros addObject:[ZCMonitorBroadcast broadcastType:(1 << i) issuer:broadcast.issuer copy:broadcast]];
+        if (y == 1) {
+            [subbros addObject:[ZCMonitorBroadcast broadcastType:(1 << i) issuer:broadcast.issuer copy:broadcast]];
+        }
         i += 1;
     }
     for (ZCMonitorBroadcast *subbro in subbros) {
-        if (MONITOR_USE_PRIORITY) {[self issue_map:subbro]; continue;}
+        if (MONITOR_USE_PRIORITY_LAZY) {
+            [self issue_map:subbro]; continue;
+        }
         int rank = 0;
         for (ZCMonitorListener *listen in self.listeners) {
             if (listen.listener && (listen.listenType & subbro.type)) {
@@ -193,6 +306,18 @@
 + (void)issue_broadcast:(ZCMonitorBroadcast *)broadcast {
     NSAssert([NSThread currentThread].isMainThread, @"current is not main thread");
     [[ZCMonitorService instance] issue_api:broadcast];
+}
+
++ (void)open_lazyReceive:(id <ZCMonitorProtocol>)listener type:(ZCMonitorType)type {
+    NSAssert([NSThread currentThread].isMainThread, @"current is not main thread");
+    ZCMonitorListener *lis = [[ZCMonitorService instance] find_listener:listener];
+    if (lis) [lis open_caches:type];
+}
+
++ (void)close_lazyReceive:(id <ZCMonitorProtocol>)listener type:(ZCMonitorType)type {
+    NSAssert([NSThread currentThread].isMainThread, @"current is not main thread");
+    ZCMonitorListener *lis = [[ZCMonitorService instance] find_listener:listener];
+    if (lis) [lis close_caches:type];
 }
 
 + (void)register_listener:(id <ZCMonitorProtocol>)listener {
